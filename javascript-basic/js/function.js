e09b19578f9b8597bac60c1341f24016b34df89d
softoasis2022 <132141967+softoasis2022@users.noreply.github.com>
2026-09-02 15:20:03 +0900

    작업 내용

---
+++ javascript-basic/js/function.js
@@ -0,0 +1,23 @@
+//함수: 기능
+// 1. 함수 정의와 호출
+
+// 함수(를) 정의(한다)
+// function 함수명() {
+//     console.log("안녕하세요!");
+// }
+
+//매개변수 parameter
+function hello (str) {
+    // console.log(str);
+    return str;
+}
+
+//함수 호출
+// 함수명();
+hello();
+
+console.log("안녕하세요!");
+
+//함수 값 반환
+const result hello("메롱!@#$%");
+console.log(result);
