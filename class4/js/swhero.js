242998495f7e5a21ce093b89ac1532887709e3c5
softoasis2022 <132141967+softoasis2022@users.noreply.github.com>
2026-09-08 16:49:55 +0900

    Today

--- class4/js/swhero.js
+++ class4/js/swhero.js
@@ -1,12 +1,27 @@
-cnst main-visual-bg = document.querySelectorAll(".hero .main-visual .main-visual-bg");
+const main-visual-bg = document.querySelectorAll(".hero .main-visual .main-visual-bg");
 // console.log(mainvisualbg);
 
+const herobtns = document.querySelectorAll(".visual-inner btn");
+console.log(herobtns);
+const playstopbtn = document.querySelector(".sw-hero .pagination .play-stop-btn");
+console.log(play-stop-btn);
+// 히어로 슬라이드 버튼 호버 인터렉션
+herobtns.forEach(() => {
+  Btn.addeventlistener("mouseenter", ()=>{
+    btn.querySelector(".on").style.opacity = 1;
+  });
+  btn.addeventlistener("mouseout", () => {
+    btn.querySelector(".off").style.opacity = 0;
+  });
+});
+
+// 스와이퍼 설정
 const swHero = new Swiper(".sw-hero", {
   speed: 1000,
-  // autoplay: {
-  //   delay: 5000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -28,4 +43,20 @@ const swHero = new Swiper(".sw-hero", {
       mainvisualbg.style.backgroundimage = 'url(${activeslideimg.src})';
     },
   },
+});
+
+//재생, 정지버튼
+playstopbtn.addEventListener("click", () => {
+  swHero.autoplay.stop();
+  const isrunning = swHero.autoplay.running;
+  // console.log(slideautostop);
+
+  if(isrunning) {
+    swHero.autoplay.stop();
+    console.log("자동재생 정지");
+    playstopbtn.style.backgroundimage = 'url(asset/icon/slide_play.png)';
+  } else {
+    swHero.autoplay.start();
+    console.log("자동재생 시작")
+  }
 });
