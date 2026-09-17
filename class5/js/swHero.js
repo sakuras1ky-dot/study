11dee4dd93c35923f2676f55969cf976f48a3748
softoasis2022 <132141967+softoasis2022@users.noreply.github.com>
2026-09-17 17:05:59 +0900

    Today

--- class5/js/swHero.js
+++ class5/js/swHero.js
@@ -27,7 +27,7 @@ const swHero = new Swiper(".sw-hero", {
     disableOnInteraction: false,
   },
   pagination: {
-    el: ".swiper-pagination",
+    el: "sw-hero .swiper-pagination",
     type: "fraction",
   },
   navigation: {
